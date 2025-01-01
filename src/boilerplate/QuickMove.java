b958092b710a86bfac7351365d752ded19cda594
Jon Geiger Gajate <jongg97@gmail.com>
2025-01-01 02:53:40 +0100

    Add files via upload

--- src/boilerplate/QuickMove.java
+++ src/boilerplate/QuickMove.java
@@ -29,7 +29,7 @@ public class QuickMove {
         for (Piece p : board.getBotPieces()) {
             int x = p.getXCoordinate();
             int y = p.getYCoordinate();
-            Board boardCopy = board.getBoardDeepCopy();
+           
 
             int[][] directions = p.isKing() ? new int[][] { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } }
                     : new int[][] { { 1, 1 }, { 1, -1 } };
@@ -41,30 +41,14 @@ public class QuickMove {
                 if (!board.validMove(p, newX, newY)) {
                     continue;
                 } else {
-                    boardCopy.move(p, newX, newY);
-                    if (noBlunder(boardCopy)) {
-                        return new int[]{
-                                x, y, newX, newY
-                            };      
-                    }else if (fallback == null) {
-                        fallback = new Move(p.getXCoordinate(), p.getYCoordinate(),
-                                newX, newY);
-                    }
-                    
+                    return new int[]{
+                            x, y, newX, newY
+                        };                 
                 }
             }
            
         }
-        return fallback != null ? fallback.getMoveArray() : null;
-    }
-
-    private boolean noBlunder(Board boardCopy) {
-        for (Piece p : boardCopy.getPlayerPieces()) {
-            
-            if (boardCopy.capturePossible(p, false))
-                return false;
-        }
-        return true;
+        return  null;
     }
 
 }
